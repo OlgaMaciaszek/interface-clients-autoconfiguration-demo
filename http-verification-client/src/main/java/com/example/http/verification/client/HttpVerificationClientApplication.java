620f086c8d212a6e16eb03272d6dfda80a6cfb1d
Dave Syer <dsyer@vmware.com>
2025-02-27 13:43:19 +0000

    Add repos and an autowired
    # Conflicts:
    #	http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -1,10 +1,13 @@
 package com.example.http.verification.client;
 
+import com.example.http.verification.client.clients.PersonService;
 import com.example.http.verification.client.clients.VerificationService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.interfaceclients.http.InterfaceClientGroup;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @InterfaceClientGroup(value = "verificationClient", basePackageClasses = VerificationService.class)
@@ -13,6 +16,13 @@ public class HttpVerificationClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HttpVerificationClientApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(PersonService service) {
+		return args -> {
+			System.err.println(service.test());
+		};
 	}
 
 }
