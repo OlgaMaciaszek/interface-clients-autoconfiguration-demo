6c07a30d4a651d8c9b19f59dcd6d5841ecbbc345
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-05-13 17:15:35 +0200

    Use LB Configurer from SC Commons. Add demo properties.

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -2,20 +2,16 @@ package com.example.http.verification.client;
 
 import com.example.http.verification.client.clients.PersonService;
 import com.example.http.verification.client.clients.VerificationService;
-import com.example.http.verification.client.config.LoadBalancerWebClientHttpServiceGroupConfigurer;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.interfaceclients.http.HttpInterfaceGroupsProperties;
-import org.springframework.cloud.client.loadbalancer.reactive.DeferringLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.service.registry.HttpServiceGroup;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @SpringBootApplication
-@ImportHttpServices(value = "verificationClient", httpServiceTypes = {VerificationService.class,
+@ImportHttpServices(group = "verificationClient", types = {VerificationService.class,
 		PersonService.class}, clientType = HttpServiceGroup.ClientType.WEB_CLIENT)
 public class HttpVerificationClientApplication {
 
@@ -23,13 +19,6 @@ public class HttpVerificationClientApplication {
 		SpringApplication.run(HttpVerificationClientApplication.class, args);
 	}
 
-	@Bean
-	@Order
-	LoadBalancerWebClientHttpServiceGroupConfigurer configurer(DeferringLoadBalancerExchangeFilterFunction filterFunction,
-			HttpInterfaceGroupsProperties properties) {
-		return new LoadBalancerWebClientHttpServiceGroupConfigurer(filterFunction,
-				properties);
-	}
 
 	@Bean
 	public CommandLineRunner runner(PersonService service) {
